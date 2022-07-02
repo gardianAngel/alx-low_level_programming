9679d0be51dc61e0cf0e68f6293b1e9d60edaf10
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-02 00:05:19 -0700

    modified 5-printf.c| alx-low level programming : task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-
-/
-
+/**
  * main - Entry point
- *
- *  * Return: Always 0 (success)
- *
- *   */
-
- int main(void)
-
+ * Description: C progra that prints exacly with proper grammer
+ * Return: Always 0 (Success)
+ **/
+int main(void)
 {
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
-
 }
-
-
