491c2f1cb2b7dd8bbd1d7724942da878e3a91869
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-02 01:07:39 -0700

    modified 6-size.c for betty style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,41 +1,15 @@
-#include < stdio.h >
-
-/*
+#include <stdio.h>
+/**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   * Return: Always 0 (success)
- *
- *    */
-
+ * Return: Always 0 (Succcess)
+ **/
 int main(void)
-
 {
-
-	int i;
-
-	long int l;
-
-	long long int ll;
-
-	float d;
-
-	char c;
-
-
-
-	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-
-	printf("Size of an int: %lu byte(s)\n", sizeof(i));
-
-	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
-
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-
-	printf("Size of a float: %lu byte(s)\n", sizeof(d));
-
-			return (0);
-
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
-
-
