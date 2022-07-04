3dc889f4a7250f5666a30640eaf21406796a7665
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-04 07:54:51 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -8,13 +7,14 @@
 int main(void)
 {
 	int digit1, digit2;
-	for(digit1 = 0; digit1 < 9; digit1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
+
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
-
 			if (digit1 == 8 && digit2 == 9)
 				continue;
 			putchar(',');
