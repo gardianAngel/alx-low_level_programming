f1bbee87848bc2c3b25c3124477fb1394be516f5
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-06 15:42:19 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -24,9 +24,16 @@ void jack_bauer(void)
 			if (j < 10)
 			{
 				_putchar(':');
+				_putchar('0');
+				_putchar(j + '0');
+			}
+			else if (j >= 10)
+			{
+				_putchar(':');
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
+			_putchar('\n');
 		}
 	}
 }
