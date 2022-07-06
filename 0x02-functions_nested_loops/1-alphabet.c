af1185f1ca225c41d3e25b57de9bf05eaa1b9d9b
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-05 17:49:09 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *prnt_alphabets => prints the alphabets in lowercase
+ *
+ **/
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
