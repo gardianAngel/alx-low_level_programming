0586e7e2cc792dd594c8fe29836b4fee6abdf299
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-05 19:30:20 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_abs -> returns absolut value of a number
+ *@n: parameter
+ *Return: absolute value of a number
+ *
+ **/
+
+int _abs(int)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
+		
+
