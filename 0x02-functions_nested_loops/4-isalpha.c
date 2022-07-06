aaa97c2c042a0baee5d4749e8d5c06abd162747c
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-05 18:59:25 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha -> checks for alphabet characters
+ * @c: a character arguement
+ * Return: returns 1 and 0 depending on condition
+ **/
+
+int _isalpha(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
