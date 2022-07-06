4df77160aefb7adc9debffea99eb3131a64f878a
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-05 18:22:02 -0700

    task 2 - Hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,16 @@
 void print_alphabet_x10(void)
 {
 	int x;
+	int j;
 
 	while (x < 10)
 	{
-		int j;
 
 		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
+		_putchar('\n');
 		x++;
 	}
-	_putchar('\n');
 }
