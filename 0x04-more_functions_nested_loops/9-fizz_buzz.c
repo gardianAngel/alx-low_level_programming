92c3ebea4e860dcb5cae512dbdfa1501d9e551c2
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-08 17:28:31 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,10 +12,10 @@ int main(void)
 
 	while (i <= 100)
 	{
-
-		if (i % 3 == 0)
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
 			printf("Fizz ");
-
 		else if (i % 5 == 0)
 			printf("Buzz ");
 		else
