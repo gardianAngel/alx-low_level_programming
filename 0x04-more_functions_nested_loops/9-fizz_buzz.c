ad6a3860dadc45d0daea1ac5ca4d6cea70c33201
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-08 17:10:45 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -> prints Fizz for multiples of 3 and Buzz for multiples of 5.
+ *
+ * Return: resullt
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+
+		if (i % 3 == 0)
+			printf("Fizz ");
+
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+		printf("%d ", i);
+		i++;
+	}
+	printf("\n");
+
+}
