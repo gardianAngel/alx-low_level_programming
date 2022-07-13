9fa9290dc434552d9b576bb6a24ca85c15971eff
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-13 15:45:27 -0700

    task 0 :98 Battery st

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int -> function to swap two integers
+ *
+ * @a: first  pointer
+ * @b: second pointer
+ *
+ * Returns: void
+ */
+
+void swap_int(int *a, int *b);
+{
+	int  temp = *a;
+		*a = *b;
+		*b = temp;
+}
