dd9db295815b2cc4366be8874c271c933156583a
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-13 18:12:20 -0700

    task 2 : This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen -> a function that  returns the lenght of a string.
+ * @s: points for the string to be chacked
+ * REtur: void
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		return (i);
+}
