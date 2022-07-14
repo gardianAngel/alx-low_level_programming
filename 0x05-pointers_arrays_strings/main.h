bbfac64abf9255753501e93ef69ea0ec1d5b08b9
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-13 19:42:02 -0700

    task 5 : A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,4 +6,9 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
 #endif
