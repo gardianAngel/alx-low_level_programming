550251e18565f2b7cbccd20355cee33501009071
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-13 19:24:25 -0700

    created main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,9 @@
+#ifndef main_h
+#define main_h
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
+#endif
