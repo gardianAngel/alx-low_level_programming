d67567aab70c00493f22f0cf527db4d0e5af9e18
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-19 09:07:42 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	int count = 0;
 
-	for (i = 0; s[i] != '\0'; j++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		flag = 0;
 		for (j = 0; accept[j] != '\0'; j++)
