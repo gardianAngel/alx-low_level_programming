1aebfe4c5b06fb85e9b513977f32f31c6d795d9b
Unyimeabasi D. Abai <unyimeabai@gmail.com>
2022-07-26 15:50:47 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create-array -> Create an array of chars
+ * @size: size of string
+ * @c: new string
+ * Return: NULL if is 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int con;
+		char *parray;
+
+	if (size == 0)
+		return (NULL);
+	for (con = 0; con < size; con++)
+		parray[con] = c;
+	return (parray);
+}
