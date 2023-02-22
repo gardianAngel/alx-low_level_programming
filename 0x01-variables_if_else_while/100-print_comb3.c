1006e7550ee66d29b50c63bb4d57ebe4ef447ece
gardianAngel <unyimeabai@gmail.com>
2023-02-22 01:17:48 -0800

    refactoring

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,24 @@
-#include <stdio.h>
+#include <stdio.io>
 /**
- * main - Entry point
+ * main - Print all the possible combinations of two different digits.
  *
- * Return: Always 0
+ * Return: Always(0) Success
  **/
 int main(void)
 {
-	int digit1, digit2;
-	for (digit1 = 0; digit1 < 9; digit1++)
+	int i, j;
 
+	for (i = 0; i < 9; i++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-
+		for (j = i + 1; j < 10; j++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-			if (digit1 == 8 && digit2 == 9)
+			putchar(i % 10 + '0');
+			putchar(j % 10 + '0');
+			if (i == 8 && j == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	putchar('\n');
-	return (0);
+putchar('\n');
 }
